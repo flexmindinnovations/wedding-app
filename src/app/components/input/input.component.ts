6d8c4420dd75fb10383ef398fb40a1e3d17e33c6
Imran <flexmindinnovations@outlook.com>
2024-04-19 19:14:43 +0530

    Update input.component.ts

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -100,7 +100,7 @@ export class InputComponent implements OnInit, AfterViewInit, OnDestroy, Control
     // this.disabled = isDisabled;
   }
   handleOnChange(event: any, src?: string) {
-    const value = event;
+    const value = event?.target?.value ? event?.target?.value : event;
     this.hasValue = !!value;
     const formattedValue = src === 'dt' ? new Date(value).toLocaleDateString('en-GB') : value;
     this.inputValue.emit(formattedValue);
